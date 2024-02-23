fcb531d8d23a7d03dd5f66d0b91d114c78c95c31
Abhishek kanaujia <Kanaujia.abhishek20@gmail.com>
2024-02-23 17:49:44 +0530

    Update API.ts

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from 'axios';
 
 const API: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+    baseURL: "http://3.6.14.77:8080/api/v1"
 });
 
 export default API;
